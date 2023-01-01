6744071e4271dcb128ba74826dfa40f4d2495b9b
a0171610 <ozaki.m.mi.ys@gmail.com>
2023-01-01 09:50:27 +0900

    :bug:

--- nisl_module.f90
+++ nisl_module.f90
@@ -1,7 +1,7 @@
 module nisl_module
 
   use grid_module, only: nlon, nlat, ntrunc, nz, lon, coslatr, height, dh, &
-    gu, gv, gw, gphi, gphi_initial, longitudes=>lon, latitudes=>lat, wgt
+    gu, gv, gw, gphi, gphi_initial, longitudes=>lon, latitudes=>lat, wgt, sphi_old
   private
   
   real(8), dimension(:, :, :), allocatable, private :: &
@@ -92,17 +92,15 @@ contains
     use legendre_transform_module, only: legendre_analysis, legendre_synthesis, &
         legendre_synthesis_dlon, legendre_synthesis_dlat, legendre_synthesis_dlonlat
     use interpolate2d_module, only: interpolate2d_set, interpolate2d_setd, interpolate2d_bicubic
-    use spline_interpolate_module, only: interpolate_spline_1index
 
     implicit none
 
     integer(8) :: i, j, k
     real(8), intent(in) :: t, dt
-    real(8) :: ansf(nz), f(nz)
-    real(8), allocatable :: zdot(:, :, :), midh(:, :, :)
+    real(8) :: ans1, ans2
     integer(8), allocatable :: id(:, :, :)
 
-    allocate(zdot(nlon, nlat, nz), id(nlon, nlat, nz), midh(nlon, nlat, nz))
+    allocate(id(nlon, nlat, nz))
 
     select case(case)
       case('hadley')
@@ -120,14 +118,12 @@ contains
         do j = 1, nlat
           call check_height(depheight(i, j, k))
           id(i, j, k) = int(anint(depheight(i, j, k) / dh)) + 1
-          zdot(i, j, k) = gw(i, j, k) - (height(k) - height(id(i, j, k))) / (2.0d0 * dt)
-          midh(i, j, k) = (height(k) + height(id(i, j, k))) / 2.0d0
-          call check_height(midh(i, j, k))
         enddo
       enddo
     enddo
 
-    call fd_derivative
+    call fd_derivative(gphi_old)
+    call mid_points
 
     ! set grids
     call interpolate2d_set(gphi_old)
@@ -147,12 +143,15 @@ contains
     gphiz(:, :, 1) = (gphi1(:, :, 2) - gphi1(:, :, 1)) / (height(2) - height(1))
     gphiz(:, :, nz) = (gphi1(:, :, nz) - gphi1(:, :, nz - 1)) / (height(nz) - height(nz-1))
 
-    do i = 1, nlon
-      do j = 1, nlat
-        f(:) = gphiz(i, j, :)
-        call interpolate_spline_1index(nz, height, f, midh(i,j,:), ansf)
-        do k = 1, nz
-          gphi(i, j, k) = gphi(i, j, k) + (height(k) - height(id(i, j, k))) * ansf(k)
+    call fd_derivative(gphiz)
+    call interpolate2d_set(gphiz)
+    call interpolate2d_setd(gphix, gphiy, gphixy)
+    do k = 1, nz
+      do i = 1, nlon
+        do j = 1, nlat
+          call interpolate2d_bicubic(midlon(i,j,k), midlat(i,j,k), height(k), ans1)
+          call interpolate2d_bicubic(midlon(i,j,k), midlat(i,j,k), height(id(i,j,k)), ans2)
+          gphi(i, j, k) = gphi(i, j, k) + (height(k) - height(id(i, j, k))) * (ans1 + ans2) * 0.5d0
         enddo
       enddo
     enddo
@@ -165,13 +164,15 @@ contains
       end do
     end do
 
-    do i = 1, nlon
-      do j = 1, nlat
-        f = gphiz(i, j, :)
-        call interpolate_spline_1index(nz, height, f, midh(i,j,:), ansf)
-        do k = 1, nz
-          call check_height(midh(i, j, k))
-          gphi(i, j, k) = gphi(i, j, k) - ansf(k) * dt
+    call fd_derivative(gphiz)
+    call interpolate2d_set(gphiz)
+    call interpolate2d_setd(gphix, gphiy, gphixy)
+    do k = 1, nz
+      do i = 1, nlon
+        do j = 1, nlat
+          call interpolate2d_bicubic(midlon(i,j,k), midlat(i,j,k), height(k), ans1)
+          call interpolate2d_bicubic(midlon(i,j,k), midlat(i,j,k), height(id(i,j,k)), ans2)
+          gphi(i, j, k) = gphi(i, j, k) - (ans1 + ans2) * 0.5d0 * dt
         enddo
       enddo
     enddo
@@ -191,36 +192,79 @@ contains
     endif
   end subroutine check_height
 
-  subroutine fd_derivative
+  subroutine mid_points
+    use sphere_module, only: lonlat2xyz
+    use math_module, only: pi2=>math_pi2
+    implicit none
+    integer(8) :: i, j, k
+    real(8) :: xg, yg, zg, xr, yr, zr, xm, ym, zm
+
+    do k = 1, nz
+      do i = 1, nlon
+        do j = 1, nlat
+          call lonlat2xyz(lon(i), latitudes(j), xg, yg, zg)
+          call lonlat2xyz(deplon(i, j, k), deplat(i, j, k), xr, yr, zr)
+          xm = (xg + xr) * 0.5d0
+          ym = (yg + yr) * 0.5d0
+          zm = (zg + zr) * 0.5d0
+          midlon(i, j, k) = modulo(atan2(ym, xm) + pi2, pi2)
+          midlat(i, j, k) = asin(zm / sqrt(xm**2 + ym**2 + zm**2))
+        enddo
+      enddo
+    enddo
+  end subroutine mid_points
+
+  subroutine fd_derivative(f)
     use math_module, only: pir=>math_pir, pih=>math_pih
     implicit none
+    real(8), intent(in) :: f(nlon, nlat, nz)
     integer(8) :: i, j, k
     real(8) :: dlonr, eps, gphitmp(nlon)
 
     do k = 1, nz
       dlonr = 0.25d0*nlon*pir
-      gphix(1,:,k) = dlonr * (gphi_old(2,:,k) - gphi_old(nlon,:,k))
-      gphix(nlon, :, k) = dlonr * (gphi_old(1,:,k) - gphi_old(nlon-1,:,k))
+      gphix(1,:,k) = dlonr * (f(2,:,k) - f(nlon,:,k))
+      gphix(nlon, :, k) = dlonr * (f(1,:,k) - f(nlon-1,:,k))
       do i=2, nlon-1
-        gphix(i,:,k) = dlonr*(gphi_old(i+1,:,k) - gphi_old(i-1,:,k))
+        gphix(i,:,k) = dlonr*(f(i+1,:,k) - f(i-1,:,k))
       end do
     end do
 
     do k = 1, nz
       eps = pih-latitudes(1)
-      gphitmp = cshift(gphi_old(:,1,k),nlon/2)
-      gphiy(:,1,k) = (gphitmp-gphi_old(:,2,k))/(pih+eps-latitudes(2))
+      gphitmp = cshift(f(:,1,k),nlon/2)
+      gphiy(:,1,k) = (gphitmp-f(:,2,k))/(pih+eps-latitudes(2))
       gphitmp = cshift(gphix(:,1,k),nlon/2)
       gphixy(:,1,k) = (gphitmp-gphix(:,2,k))/(pih+eps-latitudes(2))
-      gphitmp = cshift(gphi_old(:,nlat,k),nlon/2)
-      gphiy(:,nlat,k) = (gphitmp-gphi_old(:,nlat-1,k))/(-pih-eps-latitudes(nlat-1))
+      gphitmp = cshift(f(:,nlat,k),nlon/2)
+      gphiy(:,nlat,k) = (gphitmp-f(:,nlat-1,k))/(-pih-eps-latitudes(nlat-1))
       gphitmp = cshift(gphix(:,nlat,k),nlon/2)
       gphixy(:,nlat,k) = (gphitmp-gphix(:,nlat-1,k))/(-pih-eps-latitudes(nlat-1))
       do j=2, nlat-1
-        gphiy(:,j,k) = (gphi_old(:,j+1,k)-gphi_old(:,j-1,k))/(latitudes(j+1)-latitudes(j-1))
+        gphiy(:,j,k) = (f(:,j+1,k)-f(:,j-1,k))/(latitudes(j+1)-latitudes(j-1))
         gphixy(:,j,k) = (gphix(:,j+1,k)-gphix(:,j-1,k))/(latitudes(j+1)-latitudes(j-1))
       end do
     end do
   end subroutine fd_derivative
+
+  subroutine sph_derivative(f)
+    use legendre_transform_module, only: legendre_analysis, legendre_synthesis, &
+        legendre_synthesis_dlon, legendre_synthesis_dlat, legendre_synthesis_dlonlat
+    implicit none
+    integer(8) :: j, k
+    real(8) :: f(nlon, nlat, nz)
+
+    do k = 1, nz
+      call legendre_analysis(f(:,:,k), sphi_old(:,:,k))
+      call legendre_synthesis_dlon(sphi_old(:, :, k), gphix(:, :, k))
+      call legendre_synthesis_dlat(sphi_old(:, :, k), gphiy(:, :, k))
+      call legendre_synthesis_dlonlat(sphi_old(:, :, k), gphixy(:, :, k))
+    enddo
+
+    do j = 1, nlat
+      gphiy(: ,j, :) = gphiy(:, j, :) * coslatr(j)
+      gphixy(:, j, :) = gphixy(:, j, :) * coslatr(j)
+    end do
+  end subroutine sph_derivative
 
 end module nisl_module
